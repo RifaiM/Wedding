c93d683091ac030fd7bc52f433b29d5062ad83da
DPR - <rifaialc@gmail.com>
2022-03-08 19:09:45 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Onclick show div - Open Invitation
 
 function myFunction() {
-    document.getElementById("panel").style.display = "block";
-    document.getElementById("panel").scrollIntoView({ behavior: "smooth" });
-    document.activeElement.blur(); //optional if you want to blur active element.
+    document.getElementById("main").style.display = "block";
+    document.getElementById("main").scrollIntoView({ behavior: "smooth" });
+    //document.activeElement.blur(); //optional if you want to blur active element.
 }
 
 
